36f1b41506cf3bff8a33dc3dbaac89c69ec462fb
Ariel Retes <yhengdesigns@gmail.com>
2018-01-25 05:07:44 +0800

    Create articles table, model and validations

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+  
+  validates :title, presence: true, length { minimum: 3, maximum: 100 }
+  validates :description, presence true, length { minimum: 3, maximum: 300 }
+  
+end  
