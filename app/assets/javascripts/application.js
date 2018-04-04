bdf754e560da8710013691264a01c157bbed4ce8
Ariel Retes <yhengdesigns@gmail.com>
2018-04-04 18:41:48 +0800

    Install bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,9 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
+
